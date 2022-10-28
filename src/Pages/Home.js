d8e2f512e506c54f99b3a73b56f4312289435694
Akinsulere <akinsulereolajobi@gmail.com>
2022-10-28 14:48:27 +0100

    Editorial work

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div className="Page">
-      <h1>You are in the Home page</h1>
+      <h1>You are in the Home page !</h1>
       <h1>Akinsulere Olajobi</h1>
     </div>
   )
